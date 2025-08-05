224377d86bebfd9c8bbf89dd579ea0dba9d31331
marict <paul.michael.curry@gmail.com>
2025-08-05 16:59:42 -0700

    Set model initialization to "scratch" in training configuration file.

--- config/train_predictor_config.py
+++ config/train_predictor_config.py
@@ -14,7 +14,7 @@ reload_reset_iters = False
 
 # Model initialization
 # init_from = "/runpod-volume/checkpoints/lnplmuzwx704ja-only_nomag_highg/ckpt_predictor_pretrain_8160_99.99acc.pt"
-# init_from = "scratch"
+init_from = "scratch"
 
 # Dataset configuration
 dataset = "dagset"
